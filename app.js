09be1a412369a19a104290fa0266fcde1b322fc9
Sandy <sdhack786@gmail.com>
2019-04-16 18:08:49 +0530

    update

--- app.js
+++ app.js
@@ -7,23 +7,27 @@
     let humidity = document.getElementById('humidity');
     let windspeed = document.getElementById('wind');
     let visibility = document.getElementById('visibility');
+    let loader = document.getElementById('loader');
 
 
 
 
     function networkApi(cityval, countryval) {
+        loader.style.display = 'block';
 
         fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityval},${countryval}&appid=a3e3b22fc9205e235a9a7eb7ac4c89ce`)
             .then(function(result) {
                 return result.json();
             })
             .then(function(data) {
-                console.log(data);
+                loader.style.display = 'none';
+
                 location.innerText = data.name;
                 humidity.innerText = data.main.humidity + " %";
                 temperature.innerText = Math.round(data.main.temp - 273) + " °C";
                 windspeed.innerText = Math.round(data.wind.speed * 1.60933999997536) + " kph";
                 visibility.innerText = Math.round(data.visibility / 1000);
+
 
             }).catch(function() {});
     }
